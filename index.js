c0f7682f116d76db823b0fd2ba9e81e112bfea20
GiovannyJukopila <jukopila.giovanny15@gmail.com>
2023-12-31 02:35:02 +0100

    port 80

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const path = require('path');
 //const port = process.env.PORT || 2083;
 const cors = require('cors');
